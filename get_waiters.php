28f0df92df0625004a464bb80f638c10b15b0289
Mash <151622462+Mashhood-Hussain@users.noreply.github.com>
2025-01-31 11:09:54 +0000

    get_waiters.php

--- get_waiters.php
+++ get_waiters.php
@@ -1,1 +1,15 @@
+<?php
+include 'db_connection.php';
 
+$sql = "SELECT * FROM waiters";
+$result = $conn->query($sql);
+
+$waiters = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $waiters[] = $row;
+    }
+}
+
+echo json_encode($waiters);
+?>
